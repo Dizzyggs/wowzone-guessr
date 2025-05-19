57496d72d3469dba7ae7f996c6eabefdd0ceb81f
FREDDY LUNDELL <freddy_lundell@hotmail.com>
2025-05-19 22:36:22 +0200

    Routing fix netlify

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import theme from './theme'
 import './index.css'
 
 // Handle Vite preload errors
@@ -17,10 +15,8 @@ window.addEventListener('vite:preloadError', (event) => {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ChakraProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
